1ddd8857bd9debbea1a066d2a3a3bc6cea19c824
Shadow24java <alejandro.ibanezcapel@gmail.com>
2025-12-08 23:37:12 +0100

    correccion de la estetica y boton retroceso

--- app/src/main/java/com/example/buscaAnimeconexionspring/MainActivity.kt
+++ app/src/main/java/com/example/buscaAnimeconexionspring/MainActivity.kt
@@ -40,6 +40,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.title = "BuscaAnime"
 
         // --------- AUTH / GOOGLE -----------
         auth = FirebaseAuth.getInstance()
